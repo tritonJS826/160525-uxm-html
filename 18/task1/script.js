a1b8ebe9d010e7172b9fc2a273e2c1bcc252dce9
tritonJS826 <triton.68@mail.ru>
2025-07-11 14:07:20 +0200

    added 18

---
+++ 18/task1/script.js
@@ -0,0 +1,21 @@
+const dayString = prompt(); 
+const dayNumber = Number(dayString); 
+
+if  (dayNumber === 1) {
+    console.log("Monday");
+} else if (dayNumber === 2) {
+    console.log("Tuesday");
+} else if (dayNumber === 3) {
+    console.log("Wednesday");
+} else if (dayNumber === 4) {
+    console.log("Thursday");
+} else if (dayNumber === 5) {
+    console.log("Friday");
+} else if (dayNumber === 6) {
+    console.log("Saturday");
+}    else if (dayNumber === 7) {
+    console.log("Sunday");
+}
+else {
+    console.log("Invalid day");
+}
